a2dd59a99809a9c07aef83373c8c7d1dab378a62
Trinity Fore <trinityfore@icloud.com>
2022-01-29 12:03:48 -0600

    push

--- server.js
+++ server.js
@@ -49,7 +49,21 @@ app.post('/bookshelf', async (req,res) => {
     }
 })
 
+app.delete('/book/:id', async (req,res) => {
+    try {
+        res.json(await Book.findByIdAndDelete (req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
+app.put('/book/:id', async(req,res)=> {
+    try{
+        res.json(await Book.findByIdAndUpdate(req.params.id, req.body, { new : true }))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
 
 
