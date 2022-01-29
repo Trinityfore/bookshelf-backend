15068f0a0c4d40a2e8ce7f69ebff1ad6b2b8ba80
Trinity Fore <trinityfore@icloud.com>
2022-01-28 20:46:36 -0600

    adds server.js in backend, connection string, env file

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-/// this is my backend 
+require ('dotenv').config()
+const (PORT=3001, DATABASE_URL ) = process.env 
+const express = require ('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const morgan = require ('morgan')
+const { application } = require('express')
+
+//database connection ///
+mongoose.connect(DATABASE_URL)
+
+mongoose.connection
+.on('open', () => console.log('connected'))
+.on('close', ()=> console.log('disconnected'))
+.on('error', ()=> console.log('error'))
+
+//models
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    image: String,
+})
+
+const Book = mongoose.model('Book, bookSchema')
